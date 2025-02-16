2155ee771e44d6b95eb549d81050bcd134c3aa79
Julio Murillo <julioamurillo10@gmail.com>
2025-02-16 17:16:26 -0500

    Feat: Add Fictures and Auth

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
